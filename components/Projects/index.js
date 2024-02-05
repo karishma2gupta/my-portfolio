ea51f3ce62b74dc5c805e0c4081cf37ca64ae556
karishma2gupta <138330411+karishma2gupta@users.noreply.github.com>
2024-02-06 01:30:11 +0530

    Add files via upload

---
+++ components/Projects/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCard';
+ 
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+         Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>ALL</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>ALL</ToggleButton>
+          }
+          <Divider />
+         
+          
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+    
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
